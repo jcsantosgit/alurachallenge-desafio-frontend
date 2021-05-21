80059b64fcc185cf7ce2d552fe96e816977a7a31
Joao Santos <joao@localhost.localdomain>
2021-05-20 22:38:40 -0300

    layout mobile condluído com menu e pesquisa

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,9 +1,9 @@
 /* Set the width of the sidebar to 250px (show it) */
 function openNav() {
-    document.getElementById("mySidepanel").style.width = "254px";
-  }
+  document.getElementById("mySidepanel").style.width = "350px";
+}
   
-  /* Set the width of the sidebar to 0 (hide it) */
-  function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
-  }
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
